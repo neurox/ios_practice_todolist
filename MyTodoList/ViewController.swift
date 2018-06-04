e7441f70ae6011d695090a207e1c2887b1624657
Juan Gómez <juanjose@ingomez.com>
2018-06-03 19:35:48 -0500

    Initial Commit

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by Juan José Gómez López on 6/3/18.
+//  Copyright © 2018 INGOMEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
